207b258d1621c25f3ff5ee2925f71021af6b4b02
Pgr1HD Æ <69685534+Zocker333HD@users.noreply.github.com>
2022-02-11 16:08:05 +0000

    dyfvy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("vid1","video08_4712.mp4",.1,.1,10)
+    encoder.encode("vid1","y2mate.com_-_Meee16_v144P.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
